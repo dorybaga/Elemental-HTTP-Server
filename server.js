a770236f75a9ac8666c456995efe30a008536d6f
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-08 18:51:55 -1000

    this is the server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 /*jshint esversion: 6 */
 const http = require('http');
 const fs = require('fs');
-var html = fs.readFileSync('public/index.html');
+var index = fs.readFileSync('public/index.html');
+var error = fs.readFileSync('public/404.html');
+var helium = fs.readFileSync('public/helium.html');
+var hydrogen = fs.readFileSync('public/hydrogen.html');
+var styles = fs.readFileSync('public/css/styles.css');
 var date = new Date().toUTCString();
 
 const server = http.createServer((req, res) => {
-
+  console.log('this is the shit we are lookinf for',req.headers);
 //handle the routes
   if (req.method === 'POST') {
     //pipe the request data to the console
@@ -15,11 +19,54 @@ const server = http.createServer((req, res) => {
 
   } else {
     //for GET requests, serve up the contents in 'index.html'
-    res.writeHead(200, {'Content-Type': "text/html"});
-    res.write(html);
-    res.end();
-  }
 
+    var uri = req.url;
+
+    switch (uri) {
+
+    case '/':
+      res.writeHead(200, {'Content-Type': "text/html"});
+      res.write(index);
+      res.end();
+      break;
+
+    case '/index.html':
+      res.writeHead(200, {'Content-Type': "text/html"});
+      res.write(index);
+      res.end();
+      break;
+
+    case '/hydrogen.html':
+      res.writeHead(200, {'Content-Type': "text/html"});
+      res.write(hydrogen);
+      res.end();
+      break;
+
+    case '/helium.html':
+      res.writeHead(200, {'Content-Type': "text/html"});
+      res.write(helium);
+      res.end();
+      break;
+
+    case '/404.html':
+      res.writeHead(404, {'Content-Type': "text/html"});
+      res.write(error);
+      res.end();
+      break;
+
+    case '/css/styles.css':
+      res.writeHead(200, {'Content-Type': "text/css"});
+      res.write(styles);
+      res.end();
+      break;
+
+    default :
+      res.writeHead(404, {'Content-Type': "text/html"});
+      res.write(error);
+      res.end();
+      break;
+    }
+  }
 });
 
 server.listen(8080, () => {
